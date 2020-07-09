5bb30c255581a52a9b5299104de65e48072493de
Tasley <tasleym@gmail.com>
2020-07-09 19:36:52 -0300

    Create event list view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render #redirect
+from core.models import Event
 
 # Create your views here.
+
+
+
+# def index(request):
+#     return redirect('/agenda/')
+
+def event_list(request):
+    ##user = request.user
+    event = Event.objects.all() #filter(user=user)
+    data = {'events':event}
+    return render(request, 'agenda.html', data)
