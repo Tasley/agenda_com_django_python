298ddfb6c03ad77c1934a7c9b06d852585f821f7
Tasley <tasleym@gmail.com>
2020-07-09 20:49:41 -0300

    Add a submit to login anda error login message

--- core/views.py
+++ core/views.py
@@ -1,15 +1,38 @@
-from django.shortcuts import render #redirect
+from django.shortcuts import render, redirect  # redirect
 from core.models import Event
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
-
 
 
 # def index(request):
 #     return redirect('/agenda/')
 
+def login_user(request):
+    return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.error(request, " : User or password is not valid.")
+            return redirect('/')
+
+@login_required(login_url='/login')
 def event_list(request):
-    ##user = request.user
-    event = Event.objects.all() #filter(user=user)
-    data = {'events':event}
+    user = request.user
+    event = Event.objects.filter(user=user)
+    data = {'events': event}
     return render(request, 'agenda.html', data)
