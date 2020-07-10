bdccc446ab83e8e8588dc2ef09ef811eb5e2d821
Tasley <tasleym@gmail.com>
2020-07-09 21:35:35 -0300

    Including the template to add new events

--- core/views.py
+++ core/views.py
@@ -36,3 +36,18 @@ def event_list(request):
     event = Event.objects.filter(user=user)
     data = {'events': event}
     return render(request, 'agenda.html', data)
+
+
+@login_required(login_url='/login')
+def event(request):
+    return render (request, 'event.html')
+
+@login_required(login_url='/login')
+def submit_event(request):
+    if request.POST:
+        title = request.POST.get('title')
+        event_date = request.POST.get('event_date')
+        description = request.POST.get('description')
+        user = request.user
+        Event.objects.create(title=title, event_date=event_date, description=description, user=user)
+    return redirect('/')
